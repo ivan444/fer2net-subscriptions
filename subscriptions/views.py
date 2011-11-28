c6ab3c75d3e692766858072c4ba321f9088468c2
Ivan Krišto <ivan.kristo@gmail.com>
2011-11-28 12:15:38 +0100

    login view

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 
 @login_required (redirect_field_name='index')
-
 def index(request):
   message = u"Hello world! ČĆŠĐŽ"
-
   return render_to_response('index.html', {'message': message})
 
+def login(request):
+  return render_to_response('login.html')
