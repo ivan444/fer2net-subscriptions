d7302c9d05670be703b37baee12a0a25bdc0718c
Robert Petranovic <robi.petranovic@gmail.com>
2011-11-28 19:13:01 +0100

    1) Login is completly fixed now: 	1) Login will remember where to go even if user fails to provide correct 	data multiple times 	2) The form will now display error if wrong password provided 	3) Added logout link to index.html 	4) Comments 2) Changed urls.py: 	1) Added "index" url 	2) If the requested view doesn't exist, site will show "index" instead of 	bailing out (aka default view)

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -4,25 +4,40 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 from django.core.context_processors import csrf
 
-@login_required (redirect_field_name='index')
+@login_required 
 def index(request):
   message = u"Hello world! ČĆŠĐŽ"
   return render_to_response('index.html', {'message': message})
 
 def loginview(request):
+  # c is dictionary used to send data to template
+  c = { 'next': 'index',          # Where to go after the login, default
+        'wrong_login': False }    # Did user provided us wrong data?
+
+  if 'next' in request.GET: 
+     c['next'] = request.GET['next'] 
+
+  
+  # generating anti-CSRF key
+  c.update(csrf(request))         
+ 
   if request.method == 'POST':
      username = request.POST['username']
      password = request.POST['password']
+
+     # in case this is wrong login, site should still remember where to go next
+     c['next'] = request.POST['next'] 
+
      user = authenticate(username = username, password = password)
      if user is not None:
        login(request, user)
-     else:
-       return redirect("login")
-      
-  c = {}
-  c.update(csrf(request))
+       return redirect (c['next'])
+
+     c['wrong_login'] = True
+        
+  # show the form 
   return render_to_response('login.html', c)
 
 def logoutview(request):
   logout(request)
-  return redirect("login")
+  return redirect("loginview")
--- urls.py
+++ urls.py
@@ -5,11 +5,14 @@ import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
+  url(r'^index$', 'subscriptions.views.index', name='index'),
   url(r'^$', 'subscriptions.views.index', name='index'),
   url(r'^login$', 'subscriptions.views.loginview', name='loginview'),
+  url(r'^logout$', 'subscriptions.views.logoutview', name='logout'),
   url(r'^admin/', include(admin.site.urls)),
   url(r'^site_media/(?P<path>[a-zA-Z\-\._0-9]+)$', 'django.views.static.serve', {'document_root': settings.SITE_ROOT + '/templates/'}, name="templates"),
   url(r'^site_media/images/(?P<path>[a-zA-Z\-\._0-9]+)$', 'django.views.static.serve', {'document_root': settings.SITE_ROOT + '/templates/images'}, name="images"),
   url(r'^site_media/js/(?P<path>[a-zA-Z\-\._0-9]+)$', 'django.views.static.serve', {'document_root': settings.SITE_ROOT + '/templates/js'}, name="js"),
+  url(r'^.*$', 'subscriptions.views.index', name='index'),
 )
 
