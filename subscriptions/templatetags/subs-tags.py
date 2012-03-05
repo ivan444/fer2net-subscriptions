f8a6bcf531fc3e3a993ec5b825d969bb1fcfedac
Ivan Krišto <ivan.kristo@gmail.com>
2012-03-05 11:33:45 +0100

    Sređen raspad staff tablice

--- subscriptions/templatetags/subs-tags.py
+++ subscriptions/templatetags/subs-tags.py
@@ -2,6 +2,11 @@ from django import template
 
 register = template.Library()
 
+def trcchars(s, n):
+  if len(s) > n: return s[0:n] + "..."
+  else: return s
+trcchars.is_safe = False
+
 def dget0(h, key):
   if key in h: return h[key]
   else: return 0
@@ -12,6 +17,7 @@ def dget(h, key):
   else: return ""
 dget.is_safe = False
 
+register.filter('trcchars', trcchars)
 register.filter('dget', dget)
 register.filter('dget0', dget0)
 
